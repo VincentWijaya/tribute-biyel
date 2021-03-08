f4da6b05480e62a9d4986ba93e99399db5038347
Vincent WIjaya <weqqss@yahoo.com>
2021-03-08 23:51:07 +0700

    finish navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import { AiOutlineClose } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import { SidebarData } from './SidebarData'
 import './Navbar.css'
+import { IconContext } from 'react-icons'
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false)
@@ -18,13 +19,14 @@ function Navbar() {
 
   return (
     <>
+    <IconContext.Provider value={{color: '#fff'}}>
       <div className='navbar'>
         <Link to='#' className='menu-bars'>
           <FaBars onClick={ showSidebar } />
         </Link>
       </div>
       <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-        <ul className='nav-menu-items'>
+        <ul className='nav-menu-items' onClick={ showSidebar }>
           <li className='navbar-toogle'>
             <Link to='#' className='menu-bars'>
               <AiOutlineClose />
@@ -41,6 +43,7 @@ function Navbar() {
           })}
         </ul>
       </nav>
+    </IconContext.Provider>
     </>
   )
 }
