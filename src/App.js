b6d889f060f1b6cb6870fc975b0a614da0f063e9
Vincent WIjaya <weqqss@yahoo.com>
2021-03-09 10:19:21 +0700

    remove unused component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import { Route, Switch, BrowserRouter as Router} from 'react-router-dom'
 
-// import Navbar from './components/Navbar'
-
 import Photobook from './pages/Photobook'
 
 function App() {
   return (
     <>
       <Router>
-        {/* <Navbar /> */}
         <Switch>
           <Route path='/' component={Photobook} />
-          {/* <Route path='/photobook' /> */}
         </Switch>
       </Router>
     </>
